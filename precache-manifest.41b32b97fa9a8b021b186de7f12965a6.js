7e771c3e64cbfed8b19166b0766e310217344fe8
Rameel Azhungal Chellattan Rehim <rameel.rehim@alshaya.com>
2019-12-28 00:33:28 +0400

    Updates

---
+++ precache-manifest.41b32b97fa9a8b021b186de7f12965a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96117c22378a6daac2172515f9f88015",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "ed45b9e12305d2cbdcf1",
+    "url": "/robofriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "517bc32f3452eaedfbd0",
+    "url": "/robofriend/static/css/main.e8cb6dcb.chunk.css"
+  },
+  {
+    "revision": "ed45b9e12305d2cbdcf1",
+    "url": "/robofriend/static/js/2.2b47b015.chunk.js"
+  },
+  {
+    "revision": "517bc32f3452eaedfbd0",
+    "url": "/robofriend/static/js/main.161f11dc.chunk.js"
+  },
+  {
+    "revision": "75cc8018399ebb82c64f",
+    "url": "/robofriend/static/js/runtime~main.f5f06c82.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
